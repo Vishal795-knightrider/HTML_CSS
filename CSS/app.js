8bc58db493c1e48c7f233256a237aca7a4bc4dbc
Vishal795-knightrider <vk3293801@gmail.com>
2026-02-18 23:15:09 +0530

    added new css files

---
+++ CSS/app.js
@@ -0,0 +1,28 @@
+// function sayHello() {
+//     const name = prompt("what is you name?");
+//     document.getElementById("greeting").innerText = 'hello, ${name}How are you!';
+// }
+
+// let person = {
+//     name: "alice",
+//     age :25,
+//     isStudent: true };
+
+//     console.log(person.name);
+//     console.log(typeof person);
+
+
+//     let colors =["red", "green", "blue"];
+//     console.log(colors[0]);
+//     console.log(typeof colors);
+
+// let str2="A";
+// console.log(str2.charCodeAt(0))
+
+let a=prompt("Enter you name");
+let b=console.log(a[0]);
+let c=a.slice(1);
+
+// let x = b.touppercase();
+
+// let x= console.log(b.touppercase)
